82425f282fe18c2ea7ce015a60046fe7aa797670
wbrunovieira <wbrunovieira77@gmail.com>
2023-12-23 17:18:04 -0300

    added the navbar in the index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js ou o nome do arquivo de configuração que você está usando
 export default {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx,vue}', 
     './src/**/*.{scss,css}', 
   ],
+  theme: {
+    extend: {
+      colors: {
+        'cor-primaria': '#ed3237',
+        'cor-fundo' : '#F5F5F5',
+        'cor-texto' : '#323232',
+        'cor-complementar' : '#B48E4D',
+      },
+    },
+  },
   
 };
 
