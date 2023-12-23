802fd757bcd79c1f516c1ebd241adf8c248bb502
wbrunovieira <wbrunovieira77@gmail.com>
2023-12-23 16:54:20 -0300

    added animation to logo and change color in loading

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ import "./src/scss/tailwind.scss"
 // Importações de bibliotecas
 import barba from "@barba/core"
 import gsap from "gsap";
+function hideLoadingScreen() {
+    const loadingScreen = document.querySelector(".screen-loading");
+    if (loadingScreen) {
+        gsap.to(loadingScreen, {
+            duration: 1,
+            opacity: 0,
+            display: 'none',
+            onComplete: () => {
+                loadingScreen.style.display = 'none';
+            }
+        });
+    }
+}
+
+
+// Função para iniciar a animação da barra de carregamento
+
 
 // Inicializações de Barba.js e GSAP
 function initBarba() {
@@ -14,11 +31,23 @@ function initBarba() {
 		// Aqui você pode adicionar transições, views, etc.
 	});
 
+	
+	
 	// Aqui você pode configurar animações específicas do Barba.js com GSAP
 }
 
 // Função de inicialização principal
 function main() {
+
+	setTimeout(hideLoadingScreen, 3000);
+
+	gsap.to(".logo", {
+		duration: 3, 
+		opacity: 1,
+		scale: 1, 
+		ease: "bounce.out", // escolha um efeito de easing suave
+	});
+
 	initBarba();
 
 	// Aqui você pode adicionar qualquer outra inicialização ou lógica específica
@@ -27,3 +56,4 @@ function main() {
 
 // Carrega tudo quando o DOM estiver pronto
 document.addEventListener("DOMContentLoaded", main);
+// Função para esconder a tela de carregamento
