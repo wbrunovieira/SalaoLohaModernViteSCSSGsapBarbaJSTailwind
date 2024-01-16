9ce04360dbe3c45852b60e2e911bb4a14fe3d31d
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-16 10:29:25 -0300

    remover raycaster and mouse movements listeners

--- src/js/threeSetup.js
+++ src/js/threeSetup.js
@@ -34,34 +34,28 @@ export function initializeThreeJS() {
   renderer.setSize(width, height);
   camera.aspect = width / height;
   camera.updateProjectionMatrix();
+
   controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableZoom = false;
   controls.enableDamping = true;
   controls.dampingFactor = 0.25;
 
-  const textureLoader = new THREE.TextureLoader();
-
-  const diffuseTexture = textureLoader.load(
-    '/models/rose/textures/Red_rose_diffuse.jpeg'
-  );
-  const normalTexture = textureLoader.load(
-    'models/rose/textures/Red_rose_normal.png'
-  );
-  const occlusionTexture = textureLoader.load(
-    '/pamodels/rose/textures/Red_rose_occlusion.png'
-  );
-
   const loader = new GLTFLoader();
-
+  let rose;
   loader.load(
     '/models/rose/rose-new.glb',
     function (gltf) {
       gltf.scene.traverse(function (child) {
-        if (child.isMesh) {
-          child.material.needsUpdate = true;
-
-          console.log(child);
+        if (child.isMesh && child.name === 'rose') {
+          rose = child;
+          rose.material.needsUpdate = true;
         }
       });
+
+      if (rose) {
+        rose.rotation.y = -Math.PI / 6;
+      }
+
       scene.add(gltf.scene);
     },
     undefined,
@@ -70,27 +64,7 @@ export function initializeThreeJS() {
     }
   );
 
-  const raycaster = new THREE.Raycaster();
-  const mouse = new THREE.Vector2();
-
-  function onMouseMove(event) {
-    const bounds = renderer.domElement.getBoundingClientRect();
-    mouse.x = ((event.clientX - bounds.left) / bounds.width) * 2 - 1;
-    mouse.y = -((event.clientY - bounds.top) / bounds.height) * 2 + 1;
-
-    raycaster.setFromCamera(mouse, camera);
-
-    const intersects = raycaster.intersectObjects(scene.children, true);
-
-    if (intersects.length > 0) {
-      renderer.domElement.style.pointerEvents = 'auto';
-    } else {
-      renderer.domElement.style.pointerEvents = 'none';
-    }
-  }
-
   document.getElementById('model-container').appendChild(renderer.domElement);
-  window.addEventListener('mousemove', onMouseMove);
 
   function animate() {
     requestAnimationFrame(animate);
